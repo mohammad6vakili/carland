9e735cd3f29517619e9a115603ad29258e38c174
vakili0098007 <vakili0098007@gmail.com>
2023-11-23 09:37:59 +0330

    create header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import styles from "../../../styles/header.module.scss";
+
+const Header = () => {
+  return (
+    <>
+      <div className={styles.header}></div>
+    </>
+  );
+};
+
+export default Header;
