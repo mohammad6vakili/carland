7fe88990ea9a4f86b1140e05541ce544e86fb49d
vakili0098007 <vakili0098007@gmail.com>
2023-11-25 10:11:35 +0330

    placing content

--- src/components/main/HCarousel.jsx
+++ src/components/main/HCarousel.jsx
@@ -1,28 +1,14 @@
-import s from "../../../styles/main.module.scss";
-import MarketCard from "../main/MarketCard";
 import { Swiper, SwiperSlide } from "swiper/react";
+import styles from "../../../styles/main.module.scss";
 import { Autoplay, EffectFade, Navigation, Pagination } from "swiper/modules";
 import Image from "next/image";
 import { Button } from "reactstrap";
 import { LeftOutlined } from "@ant-design/icons";
-import { useState } from "react";
 
-const Banners = () => {
-  const [cSelected, setCSelected] = useState([]);
-
-  const onCheckboxBtnClick = (selected) => {
-    const index = cSelected.indexOf(selected);
-    if (index < 0) {
-      cSelected.push(selected);
-    } else {
-      cSelected.splice(index, 1);
-    }
-    setCSelected([...cSelected]);
-  };
-
+const HCarousel = () => {
   return (
-    <div className={s.banners}>
-      <div className={s.head_carousel}>
+    <>
+      <div className={styles.head_carousel}>
         <Swiper
           spaceBetween={30}
           effect={"fade"}
@@ -35,7 +21,7 @@ const Banners = () => {
             clickable: true,
           }}
           modules={[Autoplay, EffectFade, Navigation, Pagination]}
-          className={s.mySwiper}
+          className={styles.mySwiper}
         >
           <SwiperSlide>
             <Image
@@ -43,18 +29,18 @@ const Banners = () => {
               alt=""
               width={1000}
               height={386}
-              className={s.background}
+              className={styles.background}
             />
 
-            <div className={s.content}>
-              <div className={s.texts}>
+            <div className={styles.content}>
+              <div className={styles.texts}>
                 <h1>کارلند بهترین فروشگاه خودروی ایران</h1>
                 <p>
                   میتوانید به راحتی خودروی خود را خریداری کنید و اگر مشکلی برای
                   آن پیش آمده تمام قطعات رو اینجا پیدا کنید.
                 </p>
 
-                <Button className={s.btn}>
+                <Button className={styles.btn}>
                   لوازم ماشین
                   <div>
                     <LeftOutlined style={{ color: "#fff" }} />
@@ -67,7 +53,7 @@ const Banners = () => {
                 alt=""
                 width={386}
                 height={386}
-                className={s.car}
+                className={styles.car}
               />
             </div>
           </SwiperSlide>
@@ -78,18 +64,18 @@ const Banners = () => {
               alt=""
               width={1000}
               height={386}
-              className={s.background}
+              className={styles.background}
             />
 
-            <div className={s.content}>
-              <div className={s.texts}>
+            <div className={styles.content}>
+              <div className={styles.texts}>
                 <h1>کارلند بهترین فروشگاه خودروی ایران</h1>
                 <p>
                   میتوانید به راحتی خودروی خود را خریداری کنید و اگر مشکلی برای
                   آن پیش آمده تمام قطعات رو اینجا پیدا کنید.
                 </p>
 
-                <Button className={s.btn}>
+                <Button className={styles.btn}>
                   لوازم ماشین
                   <div>
                     <LeftOutlined style={{ color: "#fff" }} />
@@ -102,7 +88,7 @@ const Banners = () => {
                 alt=""
                 width={386}
                 height={386}
-                className={s.car}
+                className={styles.car}
               />
             </div>
           </SwiperSlide>
@@ -113,18 +99,18 @@ const Banners = () => {
               alt=""
               width={1000}
               height={386}
-              className={s.background}
+              className={styles.background}
             />
 
-            <div className={s.content}>
-              <div className={s.texts}>
+            <div className={styles.content}>
+              <div className={styles.texts}>
                 <h1>کارلند بهترین فروشگاه خودروی ایران</h1>
                 <p>
                   میتوانید به راحتی خودروی خود را خریداری کنید و اگر مشکلی برای
                   آن پیش آمده تمام قطعات رو اینجا پیدا کنید.
                 </p>
 
-                <Button className={s.btn}>
+                <Button className={styles.btn}>
                   لوازم ماشین
                   <div>
                     <LeftOutlined style={{ color: "#fff" }} />
@@ -137,7 +123,7 @@ const Banners = () => {
                 alt=""
                 width={386}
                 height={386}
-                className={s.car}
+                className={styles.car}
               />
             </div>
           </SwiperSlide>
@@ -148,18 +134,18 @@ const Banners = () => {
               alt=""
               width={1000}
               height={386}
-              className={s.background}
+              className={styles.background}
             />
 
-            <div className={s.content}>
-              <div className={s.texts}>
+            <div className={styles.content}>
+              <div className={styles.texts}>
                 <h1>کارلند بهترین فروشگاه خودروی ایران</h1>
                 <p>
                   میتوانید به راحتی خودروی خود را خریداری کنید و اگر مشکلی برای
                   آن پیش آمده تمام قطعات رو اینجا پیدا کنید.
                 </p>
 
-                <Button className={s.btn}>
+                <Button className={styles.btn}>
                   لوازم ماشین
                   <div>
                     <LeftOutlined style={{ color: "#fff" }} />
@@ -172,7 +158,7 @@ const Banners = () => {
                 alt=""
                 width={386}
                 height={386}
-                className={s.car}
+                className={styles.car}
               />
             </div>
           </SwiperSlide>
@@ -183,18 +169,18 @@ const Banners = () => {
               alt=""
               width={1000}
               height={386}
-              className={s.background}
+              className={styles.background}
             />
 
-            <div className={s.content}>
-              <div className={s.texts}>
+            <div className={styles.content}>
+              <div className={styles.texts}>
                 <h1>کارلند بهترین فروشگاه خودروی ایران</h1>
                 <p>
                   میتوانید به راحتی خودروی خود را خریداری کنید و اگر مشکلی برای
                   آن پیش آمده تمام قطعات رو اینجا پیدا کنید.
                 </p>
 
-                <Button className={s.btn}>
+                <Button className={styles.btn}>
                   لوازم ماشین
                   <div>
                     <LeftOutlined style={{ color: "#fff" }} />
@@ -207,7 +193,7 @@ const Banners = () => {
                 alt=""
                 width={386}
                 height={386}
-                className={s.car}
+                className={styles.car}
               />
             </div>
           </SwiperSlide>
@@ -218,18 +204,18 @@ const Banners = () => {
               alt=""
               width={1000}
               height={386}
-              className={s.background}
+              className={styles.background}
             />
 
-            <div className={s.content}>
-              <div className={s.texts}>
+            <div className={styles.content}>
+              <div className={styles.texts}>
                 <h1>کارلند بهترین فروشگاه خودروی ایران</h1>
                 <p>
                   میتوانید به راحتی خودروی خود را خریداری کنید و اگر مشکلی برای
                   آن پیش آمده تمام قطعات رو اینجا پیدا کنید.
                 </p>
 
-                <Button className={s.btn}>
+                <Button className={styles.btn}>
                   لوازم ماشین
                   <div>
                     <LeftOutlined style={{ color: "#fff" }} />
@@ -242,91 +228,14 @@ const Banners = () => {
                 alt=""
                 width={386}
                 height={386}
-                className={s.car}
+                className={styles.car}
               />
             </div>
           </SwiperSlide>
         </Swiper>
       </div>
-
-      <section className={s.market}>
-        <div className={s.market_filters}>
-          <div className={s.categories}>
-            <div className={s.title}>دسته بندی</div>
-            <div className={s.list}>
-              <section className={s.list_item}>خودرو</section>
-              <section className={s.list_item}>خودرو</section>
-              <section className={s.list_item}>خودرو</section>
-              <section className={s.list_item}>خودرو</section>
-            </div>
-          </div>
-
-          <div className={s.brands}>
-            <div className={s.title}> برندها</div>
-            <div className={s.list}>
-              <section className={s.list_item}>خودرو</section>
-              <section className={s.list_item}>خودرو</section>
-              <section className={s.list_item}>خودرو</section>
-              <section className={s.list_item}>خودرو</section>
-            </div>
-          </div>
-
-          <div className={s.year}>
-            <div className={s.title}>سال</div>
-            <div className={s.list}>
-              <div className={s.list_item}>
-                <Button
-                  onClick={() => onCheckboxBtnClick(2)}
-                  active={cSelected.includes(2)}
-                ></Button>{" "}
-                1402
-              </div>
-              <div className={s.list_item}>
-                <Button
-                  onClick={() => onCheckboxBtnClick(2)}
-                  active={cSelected.includes(2)}
-                ></Button>{" "}
-                1402
-              </div>
-              <div className={s.list_item}>
-                <Button
-                  onClick={() => onCheckboxBtnClick(2)}
-                  active={cSelected.includes(2)}
-                ></Button>{" "}
-                1402
-              </div>
-            </div>
-          </div>
-
-          <div className={s.price_range}>
-            <div className={s.title}>محدوده قیمت</div>
-            <div className={s.range}>
-              <span className={s.from}>۲۰,۶۰۰,۰۰۰ تومان</span>
-              <span className={s.to}>۲۰,۶۰۰,۰۰۰ تومان</span>
-            </div>
-          </div>
-
-          <div className={s.color_select}>
-            <div className={s.title}>انتخاب رنگ</div>
-            <div className={s.color_list}>
-              <div className={s.color_box}></div>
-            </div>
-          </div>
-        </div>
-
-        <div className={s.market_materials1}>
-          <MarketCard
-            // key={Math.random * index}
-            image={"/assets/main/club.png"}
-            off={"item.off"}
-            title={"item.title"}
-            description={"item.description"}
-            price={"item.price"}
-          />
-        </div>
-      </section>
-    </div>
+    </>
   );
 };
 
-export default Banners;
+export default HCarousel;
