91f49536822cd31818ec8ea7b159da5524201069
vakili0098007 <vakili0098007@gmail.com>
2023-12-22 15:27:37 +0330

    updates

--- pages/userDashboard/myAdds/index.jsx
+++ pages/userDashboard/myAdds/index.jsx
@@ -1,8 +1,8 @@
 import Footer from "@/src/components/header-footer/Footer";
 import Header from "@/src/components/header-footer/Header";
 import UDNavigation from "@/src/components/user dashboard/UDNavigation";
+import Adds from "@/src/components/user dashboard/Adds";
 import styles from "../../../styles/main.module.scss";
-import UserDashboard from "@/src/components/user dashboard/UserDashboard";
 
 const page = () => {
   return (
@@ -10,7 +10,7 @@ const page = () => {
       <Header />
       <div className={styles.userDashboard_container}>
         <UDNavigation />
-        <UserDashboard />
+        <Adds />
       </div>
       <Footer />
     </>
--- src/components/user dashboard/Adds.jsx
+++ src/components/user dashboard/Adds.jsx
@@ -3,7 +3,11 @@ import s from "../../../styles/main.module.scss";
 const UserData = () => {
   return (
     <>
-      <div className={s.adds}></div>
+      <div className={s.adds}>
+        <section className={s.myAdds}></section>
+
+        <section className={s.banner}></section>
+      </div>
     </>
   );
 };
