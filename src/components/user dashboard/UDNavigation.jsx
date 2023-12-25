55e7b7cb0303b757b04b3ccede291bdb597d4fae
vakili0098007 <vakili0098007@gmail.com>
2023-12-26 01:59:53 +0330

    update for navigation mobile

--- src/components/user dashboard/UDNavigation.jsx
+++ src/components/user dashboard/UDNavigation.jsx
@@ -246,10 +246,220 @@ const UDNavigation = () => {
         </div>
       ) : (
         <>
-          <Button onClick={() => handleNavColl()}>
-            <LeftOutlined />
+          <Button className={s.nav_call_btn} onClick={() => handleNavColl()}>
+            <div>
+              <LeftOutlined />
+              <LeftOutlined />
+            </div>
           </Button>
-          <Offcanvas direction="end" scrollable toggle={navColl}></Offcanvas>
+          <Offcanvas
+            direction="end"
+            scrollable
+            isOpen={!navColl}
+            toggle={() => handleNavColl()}
+          >
+            <div className={s.userDashboard_navigation}>
+              <Image className={s.circle1} src={circle1} alt="" />
+              <Image className={s.circle2} src={circle2} alt="" />
+
+              <div className={s.name_profile}>
+                <div className={s.profile}>
+                  <Image
+                    src={"/assets/userDashboard/profile.png"}
+                    alt="profile"
+                    width={60}
+                    height={60}
+                  />
+                </div>
+                <div className={s.name}>
+                  <span>پروفایل</span>
+                  <p> مهزیار رازه </p>
+                </div>
+              </div>
+
+              <div className={s.wallet}>
+                <Image src={wallet} alt="" />
+
+                <div className={s.budget}>
+                  <span>موجودی کیف پول</span>
+                  <div>
+                    <span>۱۰۰۰۰۰۰</span> تومان
+                  </div>
+                </div>
+
+                <Button>
+                  <GoPlus style={{ color: "#fff" }} />
+                </Button>
+              </div>
+
+              <div className={s.routes}>
+                <Link
+                  href={"/userDashboard/"}
+                  className={handleSelectedRoute("") ? s.selected : s.route}
+                >
+                  <div className={s.background}>
+                    <Image src={selectedBackground} alt="" />
+                  </div>
+                  <div className={s.icon}>
+                    <MdDashboard style={{ borderRadius: "10px" }} />
+                  </div>
+                  <span>داشبورد</span>
+                </Link>
+
+                <Link
+                  href={"/userDashboard/userData"}
+                  className={
+                    handleSelectedRoute("userData") ? s.selected : s.route
+                  }
+                >
+                  <div className={s.background}>
+                    <Image src={selectedBackground} alt="" />
+                  </div>
+                  <div className={s.icon}>
+                    <BsPersonFill style={{ borderRadius: "10px" }} />
+                  </div>
+                  <span>اطلاعات حساب کاربری</span>
+                </Link>
+
+                <Link
+                  href={"/userDashboard/wallet"}
+                  className={
+                    handleSelectedRoute("wallet") ? s.selected : s.route
+                  }
+                >
+                  <div className={s.background}>
+                    <Image src={selectedBackground} alt="" />
+                  </div>
+                  <div className={s.icon}>
+                    <PiWalletFill style={{ borderRadius: "10px" }} />
+                  </div>
+                  <span>کیف پول</span>
+                </Link>
+
+                <Link
+                  href={"/userDashboard/myAdds"}
+                  className={
+                    handleSelectedRoute("myAdds") ? s.selected : s.route
+                  }
+                >
+                  <div className={s.background}>
+                    <Image src={selectedBackground} alt="" />
+                  </div>
+                  <div className={s.icon}>
+                    <IoMdMegaphone style={{ borderRadius: "10px" }} />
+                  </div>
+                  <span>آگهی‌های من</span>
+                </Link>
+
+                <Link
+                  href={"/userDashboard/myJobs"}
+                  className={
+                    handleSelectedRoute("myJobs") ? s.selected : s.route
+                  }
+                >
+                  <div className={s.background}>
+                    <Image src={selectedBackground} alt="" />
+                  </div>
+                  <div className={s.icon}>
+                    <TbBriefcaseFilled style={{ borderRadius: "10px" }} />
+                  </div>
+                  <span>مشاغل من</span>
+                </Link>
+
+                <Link
+                  href={"/userDashboard/myComments"}
+                  className={
+                    handleSelectedRoute("myComments") ? s.selected : s.route
+                  }
+                >
+                  <div className={s.background}>
+                    <Image src={selectedBackground} alt="" />
+                  </div>
+                  <div className={s.icon}>
+                    <HiChatAlt style={{ borderRadius: "10px" }} />
+                  </div>
+                  <span>نظرات من</span>
+                </Link>
+
+                <Link
+                  href={"/userDashboard/myOrders"}
+                  className={
+                    handleSelectedRoute("myOrders") ? s.selected : s.route
+                  }
+                  style={{ borderBottom: "1px solid #E6E6E6" }}
+                >
+                  <div className={s.background}>
+                    <Image src={selectedBackground} alt="" />
+                  </div>
+                  <div className={s.icon}>
+                    <Image src={shoppingBag} alt="" />
+                  </div>
+                  <span>سفارش‌ها</span>
+                </Link>
+
+                <Link
+                  href={"/userDashboard/transaction"}
+                  className={
+                    handleSelectedRoute("transaction") ? s.selected : s.route
+                  }
+                >
+                  <div className={s.background}>
+                    <Image src={selectedBackground} alt="" />
+                  </div>
+                  <div className={s.icon}>
+                    <Image src={transaction} alt="" />
+                  </div>
+                  <span>تراکنش‌ها</span>
+                </Link>
+
+                <Link
+                  href={"/userDashboard/myTickets"}
+                  className={
+                    handleSelectedRoute("myTickets") ? s.selected : s.route
+                  }
+                >
+                  <div className={s.background}>
+                    <Image src={selectedBackground} alt="" />
+                  </div>
+                  <div className={s.icon}>
+                    <Image src={ticket} alt="" />
+                  </div>
+                  <span>تیکت‌های من</span>
+                </Link>
+
+                <Link
+                  href={"/userDashboard/supports"}
+                  className={
+                    handleSelectedRoute("supports") ? s.selected : s.route
+                  }
+                >
+                  <div className={s.background}>
+                    <Image src={selectedBackground} alt="" />
+                  </div>
+                  <div className={s.icon}>
+                    <Image
+                      src={support}
+                      alt=""
+                      style={{ borderRadius: "10px" }}
+                    />
+                  </div>
+                  <span>پشتیبانی</span>
+                </Link>
+
+                <div className={s.signout}>
+                  <Button
+                    onClick={() => setSignoutModal(!signoutModal)}
+                    className={s.btn}
+                  >
+                    <div className={s.icon}>
+                      <ImExit />
+                    </div>
+                    <span>خروج</span>
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </Offcanvas>
         </>
       )}
 
