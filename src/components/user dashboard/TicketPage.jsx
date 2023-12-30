e40699d7de3416910ddb465c0c199a8f93e173dc
vakili0098007 <vakili0098007@gmail.com>
2023-12-30 20:07:44 +0330

    ticket page

--- src/components/user dashboard/TicketPage.jsx
+++ src/components/user dashboard/TicketPage.jsx
@@ -1,6 +1,15 @@
+import { usePathname } from "next/navigation";
 import s from "../../../styles/main.module.scss";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const TicketPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log(router.query.ticketId);
+  }, []);
+
   return (
     <>
       <div className={s.ticket_page}></div>
